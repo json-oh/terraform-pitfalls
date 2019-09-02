6e8a7e2b3e8531bb6178e45bd363289878c4f07d
millenial-dev <ojs2643@gmail.com>
2019-09-02 11:23:26 +0900

    모듈 사용 시 output 출력과 remote state 저장 추가
    (cherry picked from commit db413d18f20578018012bc71e3db933d2372f1c8)

---
+++ stack/dev/ec2_monitor/outputs.tf
@@ -0,0 +1,3 @@
+output "ec2_monitor_queue_arn" {
+  value = module.ec2_monitor.ec2_monitor_queue_arn
+}
---
+++ stack/dev/ec2_monitor/terraform.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    region         = "ap-northeast-2"
+    bucket         = "terraform-state"
+    key            = "dev/ec2_monitor/terraform.tfstate"
+    dynamodb_table = "terraform-state-lock"
+    role_arn       = "arn:aws:iam::123456789012:role/TerraformAccess"
+  }
+}
